423a53191a3bd6eec22ff31d4402e54cbf3a7766
poojashyam-cloud <poojahepzy0@gmail.com>
2026-02-17 00:02:30 +0530

    in progress

--- Test_task12/dynamic_xpath_test.py
+++ Test_task12/dynamic_xpath_test.py
@@ -3,15 +3,21 @@ from selenium.webdriver.common.by import By
 
 def test_option1(launch_guvi):
     driver = launch_guvi
-    first_elem = driver.find_element(By.XPATH,"//div[@id='solutions']/child::p[.='LIVE Classes']").click()
-   # print("The identified Element is ",first_elem)
+    first_elem = driver.find_element(By.XPATH,"//div[@id='solutions']/child::p[.='LIVE Classes']")
+    first_elem.click()
+    text_first_elem = first_elem.text
+    print("\nThe identified Element is ",text_first_elem)
     first_child = driver.find_element(By.XPATH,"(//div[@class='⭐️f6lmuc-0 flex items-center gap-2']/child::p)[1]").text
-    print("\nFirst child for the element LIVE Classes is ",first_child)
+    print("\nFirst child for the element ",text_first_elem ,"is ",first_child)
+    #sibling
     sibling = driver.find_element(By.XPATH,"//p[.='Software Development ']/following-sibling::span").text
     print("Sibling is ",sibling)
+    #fetching all Children
     print("All children associated with the Element")
-    all_children = driver.find_elements(By.XPATH,"//div[@class='⭐️f6lmuc-0 flex items-center gap-2']/child::p")
-    for child in all_children:
-        all= driver.find_element(child).text
-        print(all)
+    all_child_list = driver.find_elements(By.XPATH,"//div[@class='⭐️f6lmuc-0 flex items-center gap-2']/child::p")
+    all_text = []
+    for child in all_child_list:
+        text = child.text
+        all_text.append(text)
+        print(text)
     driver.close()
