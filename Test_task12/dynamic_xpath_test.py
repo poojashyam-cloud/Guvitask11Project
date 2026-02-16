fe9b0d50ef01af727d14b8d2d3d48c021e41e652
poojashyam-cloud <poojahepzy0@gmail.com>
2026-02-16 22:22:08 +0530

    in progress

--- Test_task12/dynamic_xpath_test.py
+++ Test_task12/dynamic_xpath_test.py
@@ -3,8 +3,15 @@ from selenium.webdriver.common.by import By
 
 def test_option1(launch_guvi):
     driver = launch_guvi
-    time.sleep(3)
-    parent = driver.find_element(By.XPATH,"//p[@class='⭐️f6lmuc-0 menu-hover text-sm font-medium text-nowrap leading-6'][normalize-space()='LIVE Classes']").click()
-    print(parent)
-    child = driver.find_element(By.XPATH,"//p[normalize-space()='Software Development']").text
-    print(child)
+    first_elem = driver.find_element(By.XPATH,"//div[@id='solutions']/child::p[.='LIVE Classes']").click()
+   # print("The identified Element is ",first_elem)
+    first_child = driver.find_element(By.XPATH,"(//div[@class='⭐️f6lmuc-0 flex items-center gap-2']/child::p)[1]").text
+    print("\nFirst child for the element LIVE Classes is ",first_child)
+    sibling = driver.find_element(By.XPATH,"//p[.='Software Development ']/following-sibling::span").text
+    print("Sibling is ",sibling)
+    print("All children associated with the Element")
+    all_children = driver.find_elements(By.XPATH,"//div[@class='⭐️f6lmuc-0 flex items-center gap-2']/child::p")
+    for child in all_children:
+        all= driver.find_element(child).text
+        print(all)
+    driver.close()
